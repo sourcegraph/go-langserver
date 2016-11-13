ad74ff7e47041bc3c03b4334e3eb6659f36bb662
Mohamed Bana <m@bana.io>
2016-11-13 21:54:35 +0000

    move messages to messages.ts from connect.ts.

--- tests/js/src/connect.ts
+++ tests/js/src/connect.ts
@@ -7,8 +7,6 @@ let filename: string = __filename.slice(__dirname.length + 1);
 const LOGGER = new Logger(filename);
 
 const PORT_WEBSOCKET = process.env.PORT_WEBSOCKET || '4389';
-const MESSAGE_NAME: string = 'some_rpc_message';
-const MESSAGE_COUNT: number = 2; // include the init message
 const FAILURE_DELAY = 10 * 1000;
 
 LOGGER.log('PORT_WEBSOCKET', PORT_WEBSOCKET);
@@ -40,67 +38,25 @@ const run: Types.TestRun = () => {
 
         let ws = new WebSocket(WS_URL);
 
-        let messageCount: number = 0;
+        let receivedMsgCount: number = 0;
         ws.on('message', (data: any, flags: { binary: boolean }) => {
             // flags.binary will be set if a binary data is received.
             // flags.masked will be set if the data was masked.
 
-            messageCount++;
-            if (messageCount === MESSAGE_COUNT) {
+            receivedMsgCount++;
+            LOGGER.log('<--recv message - data: %j, flags: %j, receivedMsgCount: %d', data, flags, receivedMsgCount);
+            if (receivedMsgCount === Messages.Test.COUNT) {
                 succesHandler();
             }
-
-            LOGGER.log('<--recv message - data: %j, flags: %j, messageCount: %d', data, flags, messageCount);
         });
 
         ws.on('open', () => {
-            let msgJsonInit = {
-                "initMessage": "2.0",
-                "id": 1,
-                "method": "initialize",
-                "params": {
-                    "rootPath": '/Users/mbana/go/src/github.com/sourcegraph/go-langserver',
-                    "InitializeParams": {
-                        "rootPath": '/Users/mbana/go/src/github.com/sourcegraph/go-langserver'
-                    }
-                },
-                "Params": {
-                    "rootPath": '/Users/mbana/go/src/github.com/sourcegraph/go-langserver'
-                },
-                "initializeParams": {
-                    "rootPath": '/Users/mbana/go/src/github.com/sourcegraph/go-langserver'
-                },
-                "InitializeParams": {
-                    "rootPath": '/Users/mbana/go/src/github.com/sourcegraph/go-langserver'
-                }
-            };
-
-            let rpcMessageInit = Messages.toRequest(msgJsonInit);
-            LOGGER.log('-->sending message: ', rpcMessageInit);
-
-            ws.send(rpcMessageInit);
-
-            Array.from(Array(MESSAGE_COUNT).keys()).map((messageIndex) => {
-                let messageName = `MESSAGE_NAME-${messageIndex}`;
-
-                let URI = `URI-${messageName}`;
-                let Text = `Text-${messageName}`;
-                let msgJsonTextOpen = {
-                    "jsonrpc": "2.0",
-                    "id": 1,
-                    "method": "textDocument/didOpen",
-                    "params": {
-                        "TextDocument": {
-                            "URI": URI,
-                            "Text": Text,
-                        }
-                    }
-                };
-
-                let rpcMessageTextOpen = Messages.toRequest(msgJsonTextOpen);
-                LOGGER.log('-->sending message: ', rpcMessageTextOpen);
-
-                ws.send(rpcMessageTextOpen);
+            // send all the msgs: init and the rest...
+            let msgs = [Messages.Test.INIT, ...Messages.Test.REST];
+            msgs.map(msg => {
+                let asRequest = msg.toRequest();
+                LOGGER.log('-->sending message: ', asRequest);
+                ws.send(asRequest);
             });
         });
 
--- tests/js/src/messages.ts
+++ tests/js/src/messages.ts
@@ -1,7 +1,12 @@
+const MESSAGE_NAME: string = 'some_rpc_message';
+const COUNT: number = 2; // include the init message
+
 export class JsonRpcMessage {
-	private msg: Object;
-	constructor(msg: Object) {
+	private msg: any;
+	public name: string;
+	constructor(msg: any, name?: string) {
 		this.msg = msg;
+		this.name = name || msg.method;
 	}
 
 	public toRequest(): string {
@@ -14,16 +19,49 @@ export class JsonRpcMessage {
 	}
 }
 
-// export all the test messages
-// export const TEST_MESSAGES = [
+const INIT = new JsonRpcMessage({
+	"initMessage": "2.0",
+	"id": 1,
+	"method": "initialize",
+	"params": {
+		"rootPath": '/Users/mbana/go/src/github.com/sourcegraph/go-langserver',
+		"InitializeParams": {
+			"rootPath": '/Users/mbana/go/src/github.com/sourcegraph/go-langserver'
+		}
+	},
+	"Params": {
+		"rootPath": '/Users/mbana/go/src/github.com/sourcegraph/go-langserver'
+	},
+	"initializeParams": {
+		"rootPath": '/Users/mbana/go/src/github.com/sourcegraph/go-langserver'
+	},
+	"InitializeParams": {
+		"rootPath": '/Users/mbana/go/src/github.com/sourcegraph/go-langserver'
+	}
+});
 
-// ];
+const REST = Array.from(Array(COUNT).keys()).map(index => {
+	let name = `MESSAGE_NAME-${index}`;
 
-export const toRequest = (msg: Object) => {
-	let MSG_STR = JSON.stringify(msg);
-	let MSG_LEN = MSG_STR.length;
-	let CONTENT_LENGTH = `Content-Length: ${MSG_LEN}`;
+	let URI = `URI-${name}`;
+	let Text = `Text-${name}`;
+	let msg = {
+		"jsonrpc": "2.0",
+		"id": 1,
+		"method": "textDocument/didOpen",
+		"params": {
+			"TextDocument": {
+				"URI": URI,
+				"Text": Text,
+			}
+		}
+	};
 
-	let rpcMsg = `${CONTENT_LENGTH}\r\n\r\n${MSG_STR}`;
-	return rpcMsg;
+	return new JsonRpcMessage(msg, name);
+});
+
+export const Test = {
+	INIT,
+	REST,
+	COUNT
 };
