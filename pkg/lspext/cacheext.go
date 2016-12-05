b3826c5916c081fb5f45a7e9dee5d50948a85852
Keegan Carruthers-Smith <keegan.csmith@gmail.com>
2016-12-05 23:26:39 +0200

    lsp: Use xcache prefix

--- pkg/lspext/cacheext.go
+++ pkg/lspext/cacheext.go
@@ -2,14 +2,14 @@ package lspext
 
 import "encoding/json"
 
-// CacheGetParams is the input for 'cache/get'. The response is a
+// CacheGetParams is the input for 'xcache/get'. The response is a
 // 'CacheItem'. This cache is global to a language server. IE it is shared
 // amongst workspaces, but not amongst different language server types.
 type CacheGetParams struct {
 	Key string `json:"key"`
 }
 
-// CacheItem is the response for 'cache/get'.
+// CacheItem is the response for 'xcache/get'.
 type CacheItem struct {
 	// Value is the value stored in the cache. It is a *json.RawMessage
 	// since the type in the spec is `any`. It is nil if the value is not
@@ -17,7 +17,7 @@ type CacheItem struct {
 	Value *json.RawMessage `json:"value,omitempty"`
 }
 
-// CacheSetParams is the input for 'cache/set'. It is a notify method, so
+// CacheSetParams is the input for 'xcache/set'. It is a notify method, so
 // does not have a response.
 type CacheSetParams struct {
 	Key string `json:"key"`
