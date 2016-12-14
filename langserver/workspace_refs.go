c41855f86222bf0fd41ecaed2d27d06849c650e5
Stephen Gutekanst <stephen.gutekanst@gmail.com>
2016-12-13 16:34:30 -0800

    langserver: update TODO in workspace/xreferences regarding params.Files

--- langserver/workspace_refs.go
+++ langserver/workspace_refs.go
@@ -26,7 +26,8 @@ import (
 )
 
 func (h *LangHandler) handleWorkspaceReferences(ctx context.Context, conn JSONRPC2Conn, req *jsonrpc2.Request, params lspext.WorkspaceReferencesParams) ([]lspext.ReferenceInformation, error) {
-	// TODO(slimsag): respect params.Files
+	// TODO(slimsag): respect params.Files which will make performance in any
+	// moderately sized repository more bearable (right now these are really bad).
 
 	rootPath := h.FilePath(h.init.RootPath)
 	bctx := h.OverlayBuildContext(ctx, h.defaultBuildContext(), !h.init.NoOSFileSystemAccess)
