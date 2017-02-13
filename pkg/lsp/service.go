4eb350212c9946650bc0746e00f28e6a12b27e98
Keegan Carruthers-Smith <keegan.csmith@gmail.com>
2017-02-13 12:24:31 +0200

    references: Stream if streaming client capability is present

--- pkg/lsp/service.go
+++ pkg/lsp/service.go
@@ -18,6 +18,16 @@ type ClientCapabilities struct {
 	// Below are Sourcegraph extensions. They do not live in lspext since
 	// they are extending the field InitializeParams.Capabilities
 
+	// Streaming indicates the client supports receiving the result solely
+	// through $/partialResult notifications for requests from the client
+	// to the server.
+	//
+	// If true, the server is allowed to let the result in the final
+	// response be null.
+	//
+	// Currently a soon to be proposed extension https://github.com/sourcegraph/language-server-protocol/pull/19
+	Streaming bool `json:"streaming,omitempty"`
+
 	// XFilesProvider indicates the client provides support for
 	// workspace/xfiles. This is a Sourcegraph extension.
 	XFilesProvider bool `json:"xfilesProvider,omitempty"`
@@ -122,6 +132,16 @@ type ServerCapabilities struct {
 	DocumentRangeFormattingProvider  bool                             `json:"documentRangeFormattingProvider,omitempty"`
 	DocumentOnTypeFormattingProvider *DocumentOnTypeFormattingOptions `json:"documentOnTypeFormattingProvider,omitempty"`
 	RenameProvider                   bool                             `json:"renameProvider,omitempty"`
+
+	// Streaming indicates the server supports receiving the result solely
+	// through $/partialResult notifications for requests from the server
+	// to the client.
+	//
+	// If true, the client is allowed to let the result in the final
+	// response be null.
+	//
+	// Note: A proposed extension https://github.com/sourcegraph/language-server-protocol/pull/19
+	Streaming bool `json:"streaming,omitempty"`
 
 	// XWorkspaceReferencesProvider indicates the server provides support for
 	// xworkspace/references. This is a Sourcegraph extension.
