b5e9df7a3d046d0fa106897911e07c0b3e10b20b
Keegan Carruthers-Smith <keegan.csmith@gmail.com>
2017-12-05 11:58:26 +0200

    Add commented out failing tests for type aliases

--- langserver/langserver_test.go
+++ langserver/langserver_test.go
@@ -928,6 +928,33 @@ type Header struct {
 				},
 			},
 		},
+		"go1.9 type alias": {
+			rootURI: "file:///src/test/pkg",
+			fs: map[string]string{
+				"a.go": "package p; type A struct{ a int }",
+				"b.go": "package p; type B = A",
+			},
+			cases: lspTestCases{
+				overrideGodefHover: map[string]string{
+					"a.go:1:17": "type A struct; struct{ a int }",
+					"b.go:1:17": "type B A",
+					//"b.go:1:20": "", // Currently fails with "no identifier found"
+					//"b.go:1:21": "/src/test/pkg/a.go:1:17-1:18",
+				},
+				wantHover: map[string]string{
+					"a.go:1:17": "type A struct; struct {\n    a int\n}",
+					"b.go:1:17": "type B struct; struct {\n    a int\n}",
+					"b.go:1:20": "",
+					"b.go:1:21": "type A struct; struct {\n    a int\n}",
+				},
+				wantDefinition: map[string]string{
+					"a.go:1:17": "/src/test/pkg/a.go:1:17-1:18",
+					"b.go:1:17": "/src/test/pkg/b.go:1:17-1:18",
+					//"b.go:1:20": "", // Currently panics
+					//"b.go:1:21": "/src/test/pkg/a.go:1:17-1:18", // Currently fails with no identifier found
+				},
+			},
+		},
 	}
 	for label, test := range tests {
 		t.Run(label, func(t *testing.T) {
