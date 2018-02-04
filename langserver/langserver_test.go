eff5286a6c90f12f7a6763555536a21daad3492f
Quinn Slack <qslack@qslack.com>
2018-02-03 19:03:41 -0800

    fix symbol tests to account for new UpperCamelCase String() result
    broken by commit e4c90bba17d224a7cd82b6f7538a610b1d9d9c2d

--- langserver/langserver_test.go
+++ langserver/langserver_test.go
@@ -1466,7 +1466,7 @@ func callSymbols(ctx context.Context, c *jsonrpc2.Conn, uri lsp.DocumentURI) ([]
 	}
 	syms := make([]string, len(symbols))
 	for i, s := range symbols {
-		syms[i] = fmt.Sprintf("%s:%s:%s:%d:%d", s.Location.URI, s.Kind, qualifiedName(s), s.Location.Range.Start.Line+1, s.Location.Range.Start.Character+1)
+		syms[i] = fmt.Sprintf("%s:%s:%s:%d:%d", s.Location.URI, strings.ToLower(s.Kind.String()), qualifiedName(s), s.Location.Range.Start.Line+1, s.Location.Range.Start.Character+1)
 	}
 	return syms, nil
 }
@@ -1479,7 +1479,7 @@ func callWorkspaceSymbols(ctx context.Context, c *jsonrpc2.Conn, params lspext.W
 	}
 	syms := make([]string, len(symbols))
 	for i, s := range symbols {
-		syms[i] = fmt.Sprintf("%s:%s:%s:%d:%d", s.Location.URI, s.Kind, qualifiedName(s), s.Location.Range.Start.Line+1, s.Location.Range.Start.Character+1)
+		syms[i] = fmt.Sprintf("%s:%s:%s:%d:%d", s.Location.URI, strings.ToLower(s.Kind.String()), qualifiedName(s), s.Location.Range.Start.Line+1, s.Location.Range.Start.Character+1)
 	}
 	return syms, nil
 }
