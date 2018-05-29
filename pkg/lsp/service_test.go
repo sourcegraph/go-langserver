285930e55ee42d9cf89335cb4157d7d48db870bc
Keegan Carruthers-Smith <keegan.csmith@gmail.com>
2018-05-29 12:00:17 +0100

    lsp: Do not marshal a nil hover contents
    An empty slice in Go can be nil, but we don't want to marshal nil since the spec
    doesn't allow it.

--- pkg/lsp/service_test.go
+++ pkg/lsp/service_test.go
@@ -94,3 +94,44 @@ func TestMarkedString_MarshalUnmarshalJSON(t *testing.T) {
 		}
 	}
 }
+
+func TestHover(t *testing.T) {
+	tests := []struct {
+		data          []byte
+		want          Hover
+		skipUnmarshal bool
+		skipMarshal   bool
+	}{{
+		data: []byte(`{"contents":[{"language":"go","value":"foo"}]}`),
+		want: Hover{Contents: []MarkedString{{Language: "go", Value: "foo", isRawString: false}}},
+	}, {
+		data:          []byte(`{"contents":[]}`),
+		want:          Hover{Contents: nil},
+		skipUnmarshal: true, // testing we don't marshal nil
+	}}
+
+	for _, test := range tests {
+		if !test.skipUnmarshal {
+			var h Hover
+			if err := json.Unmarshal(test.data, &h); err != nil {
+				t.Errorf("json.Unmarshal error: %s", err)
+				continue
+			}
+			if !reflect.DeepEqual(test.want, h) {
+				t.Errorf("Unmarshaled %q, expected %+v, but got %+v", string(test.data), test.want, h)
+				continue
+			}
+		}
+
+		if !test.skipMarshal {
+			marshaled, err := json.Marshal(&test.want)
+			if err != nil {
+				t.Errorf("json.Marshal error: %s", err)
+				continue
+			}
+			if string(marshaled) != string(test.data) {
+				t.Errorf("Marshaled result expected %s, but got %s", string(test.data), string(marshaled))
+			}
+		}
+	}
+}
