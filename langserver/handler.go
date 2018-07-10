e51501c7169eee6dc839589d3a4d5d8c2bf79ded
lloiser <lukasberanek@gmx.net>
2018-07-10 10:00:02 +0200

    only run typecheck for save events when diagnostics are enabled

--- langserver/handler.go
+++ langserver/handler.go
@@ -397,7 +397,7 @@ func (h *LangHandler) Handle(ctx context.Context, conn jsonrpc2.JSONRPC2, req *j
 				// a user is viewing this path, hint to add it to the cache
 				// (unless we're primarily using binary package cache .a
 				// files).
-				if h.Config.DiagnosticsEnabled && (!h.Config.UseBinaryPkgCache || req.Method == "textDocument/didSave") {
+				if !h.Config.UseBinaryPkgCache || (h.Config.DiagnosticsEnabled && req.Method == "textDocument/didSave") {
 					go h.typecheck(ctx, conn, uri, lsp.Position{})
 				}
 			}
