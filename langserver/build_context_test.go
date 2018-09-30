415b214e2bb9f44c2e4161577aa8b0d1fd252da5
justfly <justfly.py@gmail.com>
2018-09-30 19:11:18 +0800

    fixed testing

--- langserver/build_context_test.go
+++ langserver/build_context_test.go
@@ -6,7 +6,7 @@ import (
 	"golang.org/x/tools/go/buildutil"
 )
 
-func TestContainingPackage(t *testing.T) {
+func TestContainingPackageInGOPATH(t *testing.T) {
 	bctx := buildutil.FakeContext(map[string]map[string]string{
 		"p": {
 			"a.go":      "package p",
@@ -20,7 +20,31 @@ func TestContainingPackage(t *testing.T) {
 		"/go/src/p/a_test.go": "p_test",
 	}
 	for file, wantPkgName := range tests {
-		pkg, err := ContainingPackage(bctx, file)
+		pkg, err := ContainingPackage(bctx, file, "")
+		if err != nil {
+			t.Fatal(err)
+		}
+		if pkg.Name != wantPkgName {
+			t.Errorf("%s: got pkg name %q, want %q", file, pkg.Name, wantPkgName)
+		}
+	}
+}
+
+func TestContainingPackageOutGOPATH(t *testing.T) {
+	bctx := buildutil.FakeContext(map[string]map[string]string{
+		"/home/me/p": {
+			"a.go":      "package p",
+			"a_test.go": "package p_test",
+		},
+	})
+	bctx.GOPATH = "/go"
+
+	tests := map[string]string{
+		"/home/me/p/a.go":      "p",
+		"/home/me/p/a_test.go": "p_test",
+	}
+	for file, wantPkgName := range tests {
+		pkg, err := ContainingPackage(bctx, file, "/home/me/p")
 		if err != nil {
 			t.Fatal(err)
 		}
